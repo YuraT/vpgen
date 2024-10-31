5fc9cf25c8a52e49e1757dc02adc8bfd7d68fde3
Yuri Tatishchev <itatishch@gmail.com>
2024-10-31 02:38:33 -0700

    some more authentik stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,14 @@
 <script lang="ts">
-	import { AuthForm } from "$lib/components/app/auth-form";
+	import { AuthForm } from '$lib/components/app/auth-form';
+
+	const { data } = $props();
+	const { user } = data;
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 
-<AuthForm />
+{#if user }
+	<p>Hi {user.name}</p>
+{:else}
+	<AuthForm class="p-4" />
+{/if}
---
+++ src/routes/user/+page.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+	import { invalidate, invalidateAll } from '$app/navigation';
+	import { Button } from '$lib/components/ui/button';
+
+	let { data } = $props();
+
+	function refetch() {
+		console.log("refetching");
+		invalidate((url) => {
+			console.log("invalidation url", url);
+			return true;
+		});
+		invalidateAll();
+	}
+</script>
+
+<p>
+	{JSON.stringify(data.user)}
+</p>
+
+<Button onclick={refetch}>
+	Invalidate Data
+</Button>
